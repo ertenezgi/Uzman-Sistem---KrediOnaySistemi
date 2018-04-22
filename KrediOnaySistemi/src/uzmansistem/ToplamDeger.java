8cb88cd4cf7b4e55603577bcef57f6b6c5e97ef7
Ezgi Erten <16846410+ertenezgi@users.noreply.github.com>
2018-04-22 20:47:33 +0300

    Add files via upload

---
+++ KrediOnaySistemi/src/uzmansistem/ToplamDeger.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uzmansistem;
+
+/**
+ *
+ * @author Ezgi ERTEN
+ */
+public class ToplamDeger {
+    
+    static int puan=0;
+    public ToplamDeger(int puan){
+        this.puan+=puan;
+    }
+    
+    
+}
